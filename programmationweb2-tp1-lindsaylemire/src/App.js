74e67982f8416591acadcae9122617df23d9da54
Lylindsay <lindsay_lemire@hotmail.com>
2022-06-23 20:45:55 -0400

    commencement de la grid

--- programmationweb2-tp1-lindsaylemire/src/App.js
+++ programmationweb2-tp1-lindsaylemire/src/App.js
@@ -1,7 +1,10 @@
+import iconfacebook from "../src/images/icon-facebook.svg";
+
 import Container from "./components/Container";
 import Tableausocial from "./components/Tableausocial";
-import Sectionsocial from "./components/SectionSocial";
+import Sectionsocial from "./components/Sectionsocial";
 import Text from "./components/Text";
+import IconFacebook from "./components/Iconfacebook";
 
 function App() {
   return (
@@ -11,7 +14,16 @@ function App() {
         <Text as="h2">Total Followers: 23,004</Text>
         <Tableausocial>
           <Sectionsocial as="facebook">
-            
+            <IconFacebook src={iconfacebook} alt="IconFacebook"/>
+          </Sectionsocial> 
+          <Sectionsocial as="twitter">
+            blbl
+          </Sectionsocial> 
+          <Sectionsocial as="instagram">
+            blbl
+          </Sectionsocial> 
+          <Sectionsocial as="youtube">
+            blbl
           </Sectionsocial> 
         </Tableausocial>
       </Container>
---
+++ programmationweb2-tp1-lindsaylemire/src/components/Iconfacebook.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const IconFacebook = ({src, alt, loading ="lazy"}) => {
+    return <img className='iconFb' src={src} alt={alt} loading={loading}/> ;
+}
+
+export default IconFacebook;
